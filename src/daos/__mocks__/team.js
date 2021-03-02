f18f53c5f07b3fb26e17c222cb44928c5568c043
Ulisses Lopes <ulissesdvl@yahoo.com.br>
2021-03-02 15:42:07 -0300

    Adding MockTeamDAO to tests

--- src/daos/__mocks__/team.js
+++ src/daos/__mocks__/team.js
@@ -1,5 +1,10 @@
-module.exports = {
-    get: (id) => {
-        return true;
+const Team = require("../../models/team");
+
+class MockTeamDAO {
+    get(id) {
+        if(id != 5) throw Error("Team not found");
+        return new Team(5, ["Caio Nunes", "Pedro Henrique", "Ulisses Lopes"]);
     }
 }
+
+module.exports = MockTeamDAO;
